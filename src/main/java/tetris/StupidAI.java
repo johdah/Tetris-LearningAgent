8c01d65a6a2411f78deee3ae0d6246aca2688d93
Johan Dahlberg <info@johandahlberg.com>
2013-03-08 22:47:46 +0100

    Initial commit

---
+++ src/main/java/tetris/StupidAI.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author RIK
+ */
+public class StupidAI extends TetrisAI {
+    
+
+
+
+    
+    public double rateBoard(Color[][] board) {
+        int maxHeight = getMaxHeight(board);
+        int nrOfWholes = getHoles(board, maxHeight);
+        int nrOfNonFullLines = getNonFullLines(board, maxHeight);
+        return nrOfWholes*7 + maxHeight * 10 + nrOfNonFullLines *10;
+    }
+
+    public int getMaxHeight(Color[][] board) {
+        int height = board.length;
+        int maxHeight = 0;
+        for (int w = 0; w < board[0].length; w++) {
+            for (int h = 0; h < board.length; h++) {
+                if (board[h][w] != null) {
+                    if (height - h > maxHeight) {
+                        maxHeight = height - h;
+                    }
+                    break;
+                }
+            }
+        }
+        return maxHeight;
+    }
+
+    public int getHoles(Color[][] board, int maxHeight) {
+        int nrOfWholes = 0;
+        for (int h = board.length - maxHeight; h < board.length; h++) {
+            for (int w = 0; w < board[0].length && h>0; w++) {
+                if (board[h][w] == null){//&& board[h-1][w]!=null) {
+                    for(int y=h-1; y>=0;y--){
+                        if(board[y][w]!=null){
+                         nrOfWholes++;
+                         break;
+                        }
+                    }
+                    
+                }
+                    //nrOfWholes += 1;//Math.pow((h - (board.length - maxHeight) + 1),2);
+                
+            }
+        }
+
+        return nrOfWholes;
+    }
+
+    public int getNonFullLines(Color[][] board, int maxHeight) {
+        int nrOfLines = 0;
+        for (int h = board.length - maxHeight; h < board.length; h++) {
+            for (int w = 0; w < board[0].length; w++) {
+                if (board[h][w] == null) {
+                    nrOfLines++;
+                    break;
+                }
+            }
+        }
+
+        return nrOfLines;
+    }
+}
---
+++ src/main/java/tetris/TetrisAI.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tetris;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author RIK
+ */
+public abstract class TetrisAI {
+    
+    abstract double rateBoard(Color[][] board);
+    
+        public void makeBestMove(Color[][] board, Figure figure) {
+        if (figure == null) {
+            return;
+        }
+        figure.saveState();
+        figure.unMark();
+        int b_rot = 0, b_x = 0, b_y = 0;
+        double minScore = 1000000;
+
+        for (int rot = 0; rot < 4; rot++) {
+            for (int x = 0; x < board.length; x++) {
+                if (figure.canMoveTo(x, figure.yPos, rot)) {
+
+                    figure.xPos = x;
+                    figure.orientation = rot;
+
+                    while (figure.canMoveTo(x, figure.yPos + 1, rot)) {
+                        figure.yPos++;
+                    }
+                    double score = rateFigure(figure, board);
+                    if (score < minScore) {
+                        minScore = score;
+                        b_x = figure.xPos;
+                        b_rot = figure.orientation;
+                        b_y = figure.yPos;
+                    }
+                    if (figure.canMoveTo(x + 1, figure.yPos, rot)) {
+                        figure.xPos++;
+                        score = rateFigure(figure, board);
+                        if (score < minScore) {
+                            minScore = score;
+                            b_x = figure.xPos;
+                            b_rot = figure.orientation;
+                            b_y = figure.yPos;
+                        }
+                        figure.xPos--;
+                    }
+                    if (figure.canMoveTo(x - 1, figure.yPos, rot)) {
+                        figure.xPos--;
+                        score = rateFigure(figure, board);
+                        if (score < minScore) {
+                            minScore = score;
+                            b_x = figure.xPos;
+                            b_rot = figure.orientation;
+                            b_y = figure.yPos;
+                        }
+                        figure.xPos++;
+                    }
+                    
+                    figure.undo();
+                }
+            }
+        }
+        System.out.println(" Score:" + minScore);
+        figure.undo();
+        figure.xPos = b_x;
+        figure.orientation = b_rot;
+        figure.yPos = b_y;
+        figure.mark();
+    }
+        
+            private void printBoard(Color[][] b) {
+        System.out.println("");
+        for (int h = 0; h < b.length; h++) {
+            for (int w = 0; w < b[0].length; w++) {
+                if (b[h][w] == null) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("X ");
+                }
+
+            }
+            System.out.println("");
+
+        }
+    }
+
+    public double rateFigure(Figure figure,Color[][] board){
+           double score = Double.MAX_VALUE;
+           figure.mark();
+           if (figure.isAllVisible()) 
+                 score = rateBoard(board);
+           figure.unMark();
+           return score;
+    }
+}
